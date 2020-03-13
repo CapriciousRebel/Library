6b782aa06ba1f4644fa2c8bda5e6d1fc6050f0a4
Smith <codingfreak98@Smiths-MacBook-Pro.local>
2020-03-14 01:51:36 +0530

    add client page

---
+++ client.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="intro">
+        <h1>Welcome , Client!</h1>
+    </div>
+    <div class="main">
+        <div class="card">
+            <h4>All Books</h4>
+        </div>
+
+        <div class="card">
+            <h4>My Books(checked out)</h4>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
